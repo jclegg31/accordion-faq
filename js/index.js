b19972acc5425558c1eddea373351196bfec57e0
julie_c <forechecker@gmail.com>
2023-12-13 14:16:30 -0700

    desktop version added some comments for clarity

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const accordion = document.getElementsByClassName('accordion');
-let activeAccordion = null;
+let activeAccordion = null; //no accordion is active
 
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
+    //checks if the clicked accordion is not the same as the currently active one
     if (this !== activeAccordion) {
-      //close previously opened accordion
+      //close previously opened accordion if there is an active accordion and it's not null
       if (activeAccordion) {
         activeAccordion.classList.remove('active');
         activeAccordion.querySelector('.accordion-content p').style.height =
@@ -13,10 +14,13 @@ for (i = 0; i < accordion.length; i++) {
 
       //Open the clicked accordion
       this.classList.toggle('active');
+      //This selects the <p> element inside the clicked accordion's content.
       const content = this.querySelector('.accordion-content p');
+      //It sets the height of the <p> element to its full height, effectively showing the content.
       content.style.height = content.scrollHeight + 'px';
 
-      // Update the activeAccordion reference
+      // It updates the activeAccordion variable to point
+      // to the accordion that has been clicked, making it the new active accordion.
       activeAccordion = this;
     } else {
       // Toggle the clicked accordion if it was the active one
@@ -24,7 +28,7 @@ for (i = 0; i < accordion.length; i++) {
       const content = this.querySelector('.accordion-content p');
       content.style.height = content.scrollHeight + 'px';
 
-      // Clear the activeAccordion reference
+      // Clear the activeAccordion reference because now no accordion is open.
       activeAccordion = null;
     }
   });
